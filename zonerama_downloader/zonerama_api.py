ec4880c93c309d789a1ed7a9d75e03c78fb00bbc
Jan Mach <xmach3@fi.muni.cz>
2024-02-25 16:57:00 +0100

    public albums api

--- zonerama_downloader/zonerama_api.py
+++ zonerama_downloader/zonerama_api.py
@@ -1,34 +1,39 @@
 import requests
 from bs4 import BeautifulSoup, Tag
+import re
 
 
 album_id_t = str
 folder_id_t = str
 username_t = str
 
-ZONERAMA_URL = 'https://eu.zonerama.com'
+ZONERAMA_URL = "https://eu.zonerama.com"
 
 
-# def getFolderAlbums(username: username_t, folder_id: folder_id_t) -> list[album_id_t]:
-#     # response = requests.get(f'{ZONERAMA_URL}/{username}/{folder_id}')
-#     response = requests.get('https://eu.zonerama.com/mustangove/1302305')
-#     response.raise_for_status()
+def get_user_folder_public_albums(
+    username: username_t, folder_id: folder_id_t
+) -> list[album_id_t]:
+    response = requests.get(f"{ZONERAMA_URL}/{username}/{folder_id}")
+    response.raise_for_status()
 
-#     print(response.text)
+    soup = BeautifulSoup(response.text, "lxml")
+    slide_div = soup.find(
+        attrs={"data-ajax-url": f"/Part/AlbumsInTab?tabId={folder_id}"}
+    )
+    assert isinstance(slide_div, Tag)
 
-#     tree = html.parse(StringIO(response.text))
-#     selector = CSSSelector('.list-alb')
-#     for i in selector(tree):
-#         print(i)
+    mtchs = re.findall(r'data-album-id=\\"([^\\]+)\\"', str(slide_div))
+    return mtchs
+
 
 def get_user_public_folders(username: username_t) -> list[folder_id_t]:
-    response = requests.get(f'{ZONERAMA_URL}/{username}')
+    response = requests.get(f"{ZONERAMA_URL}/{username}")
     response.raise_for_status()
-    
-    soup = BeautifulSoup(response.text, 'lxml')
-    tabs_list_div = soup.find(class_='profile-tabs-list')
+
+    soup = BeautifulSoup(response.text, "lxml")
+    tabs_list_div = soup.find(class_="profile-tabs-list")
     assert isinstance(tabs_list_div, Tag)
 
-    divs = tabs_list_div.find_all(class_='item')
+    divs = tabs_list_div.find_all(class_="item")
 
-    return [div['data-tab-id'] for div in divs]
+    return [div["data-tab-id"] for div in divs]
