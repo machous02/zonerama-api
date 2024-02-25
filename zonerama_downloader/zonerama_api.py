f0823f37de5ab3601a6af2d6c0ef9e07fcf08e89
Jan Mach <xmach3@fi.muni.cz>
2024-02-25 17:20:12 +0100

    oop wrappers

--- zonerama_downloader/zonerama_api.py
+++ zonerama_downloader/zonerama_api.py
@@ -2,6 +2,10 @@ import requests
 from bs4 import BeautifulSoup, Tag
 import re
 
+from zonerama_downloader.zonerama_gallery import ZoneramaGallery
+from zonerama_downloader.zonerama_folder import ZoneramaFolder
+from zonerama_downloader.zonerama_album import ZoneramaAlbum
+
 
 album_id_t = str
 folder_id_t = str
@@ -10,7 +14,7 @@ username_t = str
 ZONERAMA_URL = "https://eu.zonerama.com"
 
 
-def get_user_folder_public_albums(
+def _get_user_folder_public_albums(
     username: username_t, folder_id: folder_id_t
 ) -> list[album_id_t]:
     response = requests.get(f"{ZONERAMA_URL}/{username}/{folder_id}")
@@ -26,7 +30,14 @@ def get_user_folder_public_albums(
     return mtchs
 
 
-def get_user_public_folders(username: username_t) -> list[folder_id_t]:
+def get_public_folder_albums(folder: ZoneramaFolder) -> list[ZoneramaAlbum]:
+    return [
+        ZoneramaAlbum(id, folder, None)
+        for id in _get_user_folder_public_albums(folder.gallery.username, folder.id)
+    ]
+
+
+def _get_user_public_folders(username: username_t) -> list[folder_id_t]:
     response = requests.get(f"{ZONERAMA_URL}/{username}")
     response.raise_for_status()
 
@@ -37,3 +48,9 @@ def get_user_public_folders(username: username_t) -> list[folder_id_t]:
     divs = tabs_list_div.find_all(class_="item")
 
     return [div["data-tab-id"] for div in divs]
+
+
+def get_user_public_folders(gallery: ZoneramaGallery) -> list[ZoneramaFolder]:
+    return [
+        ZoneramaFolder(gallery, id) for id in _get_user_public_folders(gallery.username)
+    ]
