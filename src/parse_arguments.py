398460c05d33aa158a065dea28c6aaa1a400489e
Jan Mach <xmach3@fi.muni.cz>
2024-02-25 13:45:37 +0100

    argparse

---
+++ src/parse_arguments.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--download_type", type=str, default="zip")
+    parser.add_argument("-o", "--output", type=str, required=True)
+    parser.add_argument("-aid", "--album_id", type=str, required=True)
+    parser.add_argument("-sid", "--secret_id", type=str)
+    parser.add_argument("-v", "--videos", action="store_false")
+    parser.add_argument("-s", "--sleep", type=float, default=5.0)
+
+    return parser.parse_args()
