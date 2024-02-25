c49266f84d546726406a891b6caa104d6ad36177
Jan Mach <xmach3@fi.muni.cz>
2024-02-25 17:29:27 +0100

    album download

--- zonerama_downloader/zonerama_album.py
+++ zonerama_downloader/zonerama_album.py
@@ -1,5 +1,5 @@
 from zonerama_downloader.zonerama_folder import ZoneramaFolder
-from zonerama_downloader.zon
+from zonerama_downloader.download_album import download_album
 
 
 class ZoneramaAlbum:
@@ -20,3 +20,15 @@ class ZoneramaAlbum:
         self.folder = folder
         self.secret_id = secret_id
 
+    def download(
+        self,
+        destination_folder: str,
+        include_videos: bool = True,
+        original: bool = False,
+        av1: bool = False,
+        raw: bool = False,
+        sleep_for: float = 5.0,
+    ) -> None:
+        download_album(
+            self, include_videos, original, av1, raw, destination_folder, sleep_for
+        )
